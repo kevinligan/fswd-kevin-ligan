a7949d366242b114482dfb6379d5d49db0b81b7c
Kevin Ligan <kevinligan04@gmail.com>
2022-06-07 02:00:44 +0800

    initial main.js
    main.js

--- CS4/js/main.js
+++ CS4/js/main.js
@@ -6,13 +6,17 @@ var email = "",
     Sex = undefined,
     iAgree = false;
 
+    
     function register() {
         var validationResponse = { code: 200, message: "Sign up Sucessfully" };
-        if (email.length = 0) {
+        
+        if (email.length == 0) {
             validationResponse = { code: 400, message: "Please enter email address" };
         }
         else if (!email.match(/^w+([\.-]?w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)) {
-                validationResponse = { code: 400, message: "Email format not valid" };
-            }
-        return validationResponse;
+            validationResponse = { code: 400, message: "Email format not valid" };
         }
+
+        return validationResponse;
+        
+    }
